d1d12b1c4a6f3da871a83e8400e3aaa1143e82c3
Vladyslav Mankivskyi <mankivskiy.vlsd@gmail.com>
2025-04-21 23:43:07 +0300

    chore: install expo-file-system

--- lab_5/app/index.tsx
+++ lab_5/app/index.tsx
@@ -1,16 +1,33 @@
-import { Stack, Link } from "expo-router";
+import { Stack, Link } from 'expo-router';
+import * as FileSystem from 'expo-file-system';
+import { useEffect } from 'react';
 
-import { Button } from "~/components/Button";
-import { Container } from "~/components/Container";
-import { ScreenContent } from "~/components/ScreenContent";
+import { Button } from '~/components/Button';
+import { Container } from '~/components/Container';
+import { ScreenContent } from '~/components/ScreenContent';
+
+const APP_DATA_DIR = FileSystem.documentDirectory + 'AppData/';
 
 export default function Home() {
+  useEffect(() => {
+    (async () => {
+      const dirInfo = await FileSystem.getInfoAsync(APP_DATA_DIR);
+      if (!dirInfo.exists) {
+        console.log('AppData directory not found. Creating...');
+        await FileSystem.makeDirectoryAsync(APP_DATA_DIR, { intermediates: true });
+        console.log('AppData directory created.');
+      } else {
+        console.log('AppData directory exists.');
+      }
+    })();
+  }, []);
+
   return (
     <>
-      <Stack.Screen options={{ title: "Home" }} />
+      <Stack.Screen options={{ title: 'Home' }} />
       <Container>
         <ScreenContent path="app/index.tsx" title="Home"></ScreenContent>
-        <Link href={{ pathname: "/details", params: { name: "Dan" } }} asChild>
+        <Link href={{ pathname: '/details', params: { name: 'Dan' } }} asChild>
           <Button title="Show Details" />
         </Link>
       </Container>
